83f578f4192128c9430356f365632d9e71a0d8ec
hitaichou <holyfernandes@yahoo.co.uk>
2019-12-28 07:44:26 -0300

    Criação de classe simples com tabela de preço.

---
+++ CalculoTabelaPreco/CalculoPreco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CalculoTabelaPreco
+{
+    class CalculoPreco
+    {
+        double Total;
+        double CachorroQuente = 4.0;
+        double XSalada = 4.5;
+        double XBacon = 5.0;
+        double TorradaSimples = 2.0;
+        double Refrigerante = 1.5;
+
+        public double CalculoCachorroQuente (int vQtd)
+        {
+            return CachorroQuente * vQtd;
+        }
+        public double CalculoXsalada(int vQtd)
+        {
+            return XSalada * vQtd;
+        }
+        public double CalculoXBacon(int vQtd)
+        {
+            return XBacon * vQtd;
+        }
+        public double CalculoTorradaSimples(int vQtd)
+        {
+            return TorradaSimples * vQtd;
+        }
+        public double CalculoRefrigerante(int vQtd)
+        {
+            return Refrigerante * vQtd;
+        }
+    }
+}
--- CalculoTabelaPreco/Program.cs
+++ CalculoTabelaPreco/Program.cs
@@ -6,7 +6,45 @@ namespace CalculoTabelaPreco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double vTotal;
+
+            CalculoPreco cp = new CalculoPreco();
+
+            Console.WriteLine("Entre com o código do lanche e quantidade:");
+            string[] vet = Console.ReadLine().Split();
+            int vCodigo = int.Parse(vet[0]);
+            int vQtd = int.Parse(vet[1]);
+
+            if (vCodigo == 1)
+            {
+                vTotal = cp.CalculoCachorroQuente(vQtd);
+                Console.WriteLine("Total: " + vTotal.ToString("F2"));
+            }
+            else if (vCodigo == 2)
+            {
+                vTotal = cp.CalculoXsalada(vQtd);
+                Console.WriteLine("Total: " + vTotal.ToString("F2"));
+            }
+            else if (vCodigo == 3)
+            {
+                vTotal = cp.CalculoXBacon(vQtd);
+                Console.WriteLine("Total: " + vTotal.ToString("F2"));
+            }
+            else if (vCodigo == 4)
+            {
+                vTotal = cp.CalculoTorradaSimples(vQtd);
+                Console.WriteLine("Total: " + vTotal.ToString("F2"));
+            }
+            else if (vCodigo == 5)
+            {
+                vTotal = cp.CalculoRefrigerante(vQtd);
+                Console.WriteLine("Total: " + vTotal.ToString("F2"));
+            }
+            else
+                Console.WriteLine("Codigo não existente");
+
+
+
         }
     }
 }
